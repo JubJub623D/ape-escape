f193829524ce9ca838194caa501265096233d94e
JubJub623D <joshua.hyunki.kim@gmail.com>
2022-09-08 11:54:45 -0700

    Fixed script to run the model on an example

--- apeescape-portable/run_apeescape.py
+++ apeescape-portable/run_apeescape.py
@@ -1,19 +1,20 @@
+import os
 import tensorflow.keras as keras
 from keras import models
-from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences 
 import sys
-import fileinput
 import numpy as np
 
-thirty_two_kb = 2 * 1024 * 32
+thirty_two_kb = 1024 * 32
 
-model = keras.models.load_model('densemodel.h5')
-file_list = fileinput.input(mode='rb')
-ggrs = [ggrfile.hex(' ')[408:] for ggrfile in file_list]
-print(ggrs)
-'''tkn = Tokenizer(10000)
-sequences = pad_sequences(tkn.texts_to_sequences(ggrs), thirty_two_kb)
+model = keras.models.load_model('ape_escape.h5')
+ggrs = []
+for filename in sys.argv[1:]:
+  with open(filename, 'rb') as ggrfile:
+    hexed_file = ggrfile.read().hex(' ').split(' ')
+    ggrs.append(hexed_file[136:])
+    
+sequences = sequences = [[int(n, base=16) for n in sequence] for sequence in ggrs]
+output = model(pad_sequences(sequences, maxlen = thirty_two_kb))
+print(output)
 
-output = model(sequences)
-'''
